6a3ef8fc723191bd98958df29f856ffa11d9bf3c
nicki <nicki.faustino@stud.fh-campuswien.ac.at>
2022-04-19 11:21:19 +0200

    Screens and Nav

--- app/src/main/java/at/ac/fhcampuswien/momsrecipebook/navigation/AppNavigation.kt
+++ app/src/main/java/at/ac/fhcampuswien/momsrecipebook/navigation/AppNavigation.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import at.ac.fhcampuswien.momsrecipebook.screens.AddRecipeScreen
+import at.ac.fhcampuswien.momsrecipebook.screens.DetailScreen
+import at.ac.fhcampuswien.momsrecipebook.screens.HomeScreen
 import at.ac.fhcampuswien.momsrecipebook.screens.LoginScreen
 
 @Composable
@@ -13,6 +16,15 @@ fun AppNavigation() {
     NavHost(navController = navController, startDestination = AppScreens.LoginScreen.name) {
         composable(AppScreens.LoginScreen.name) {
             LoginScreen()
+        }
+        composable(AppScreens.HomeScreen.name){
+            HomeScreen(navController = navController)
+        }
+        composable(AppScreens.DetailScreen.name){
+            DetailScreen(navController = navController)
+        }
+        composable(AppScreens.AddRecipeScreen.name){
+            AddRecipeScreen(navController = navController)
         }
     }
 }
---
+++ app/src/main/java/at/ac/fhcampuswien/momsrecipebook/screens/AddRecipeScreen.kt
@@ -0,0 +1,8 @@
+package at.ac.fhcampuswien.momsrecipebook.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AddRecipeScreen(navController: NavController = rememberNavController()){}
